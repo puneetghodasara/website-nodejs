5950ff8ee6e59cb42f64edcabf2d513cb0ccf661
Punit Ghodasara <punit.ghodasara@sap.com>
2019-01-13 00:31:45 +0000

    Removed unnecessary JS+CSS
    Signed-off-by: Punit Ghodasara <punit.ghodasara@sap.com>

--- asset/pglib.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	//$logdata = array('pageid', 'page', 'timestamp', 'address', 'referrer', 'useragent', 'clientkey', 'clientpagecount', 'clientisnew', 'socialkey');
-
-	session_start();
-	$session_id = session_id();
-
-?>

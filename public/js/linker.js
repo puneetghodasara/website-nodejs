a84c6f403c513bb39ead1f6ea857da24a015726a
Punit Ghodasara <punit.ghodasara@sap.com>
2019-01-15 16:16:20 +0000

    Changing all links to HTTPS
    Signed-off-by: Punit Ghodasara <punit.ghodasara@sap.com>

--- public/js/linker.js
+++ public/js/linker.js
@@ -2,21 +2,22 @@ $( document ).ready(function() {
     var baseUrl = window.location.protocol + "//" + window.location.host;
 
     // This needs to always point ENTRY location
-    $("#navbar-home-only-home-link").attr("href","http://www.puneetghodasara.com/");
-    // This refers to HomePage of single cloud hosting
-    $("#navbar-home-link").attr("href",baseUrl);
+    $("#navbar-home-only-home-link").attr("href","https://www.puneetghodasara.com/");
+    // This refers to HomePage of single cloud hosting or actual homepage
+    // $("#navbar-home-link").attr("href",baseUrl);
+    $("#navbar-home-link").attr("href","https://www.puneetghodasara.com/");
 
     // Relative Navbar values
     $("#navbar-stories").attr("href","/story");
     $("#navbar-contact").attr("href","#");
 
     // Hardcoded redirection from Entry location
-    $("#selection-aws").attr("href","http://aws.puneetghodasara.com");
-    $("#selection-gcp").attr("href","http://gcp.puneetghodasara.com");
+    $("#selection-aws").attr("href","https://aws.puneetghodasara.com");
+    $("#selection-gcp").attr("href","https://gcp.puneetghodasara.com");
     $("#selection-azure").attr("href","#");
 
-    $("#breadcrumb-aws").attr("href","http://aws.puneetghodasara.com");
-    $("#breadcrumb-gcp").attr("href","http://gcp.puneetghodasara.com");
+    $("#breadcrumb-aws").attr("href","https://aws.puneetghodasara.com");
+    $("#breadcrumb-gcp").attr("href","https://gcp.puneetghodasara.com");
     $("#breadcrumb-azure").attr("href","#");
     $("#breadcrumb-default").attr("href",baseUrl);
 });

ec4464e1e26f43e4b1f719d7b58db5fa3bf1e5ab
Punit Ghodasara <punit.ghodasara@sap.com>
2019-03-28 11:07:53 +0000

    [CircleCI] Removing Vendor specific encryption and added gsutil
    Signed-off-by: Punit Ghodasara <punit.ghodasara@sap.com>

--- story.sh
+++
@@ -1,6 +0,0 @@
-ORIGIN=$(dirname $(readlink -f $0))
-echo $ORIGIN
-gcloud auth activate-service-account --key-file=$ORIGIN/virtualbytes.json
-gcloud config set project virtual-bytes
-mkdir -p $ORIGIN/story
-gsutil -m cp -r gs://story.puneetghodasara.com/* $ORIGIN/story
